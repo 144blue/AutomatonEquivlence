45b97480350665fa0c1e84b486f2597c8cfd7363
Laura Rubio <laurarubio11@hotmail.com>
2020-03-15 11:44:23 -0500

    Modelo agregado, clase Maquina y Estado

---
+++ equivalencia de automatas/equivalencia de automatas/Estado.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equivalencia_de_automatas
+{
+    class Estado
+    {
+
+
+        public String[] Estados { get; set; }
+        public int[] Salidas { get; set; }
+
+        public String NombreGrupo;
+
+        public String estado { get; set; }
+        private Estado estadoPendiente = null;
+        public Estado(String name)
+        {
+            estado = name;
+            NombreGrupo = "";
+
+        }
+
+        public bool CompararSalidas(int[] SalidasB)
+        {
+            for (int i = 0; i < Salidas.Length; i++)
+            {
+                if (!Salidas[i].Equals(SalidasB[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public bool ComparadorSubEstados(List<String> A, List<String> B)
+        {
+
+            for (int i = 0; i < B.Count(); i++)
+            {
+                var varUno = B[i];
+
+                if (!A.Contains(varUno))
+                {
+                    Console.WriteLine("VarUno " + B[i]);
+                    return false;
+                }
+
+            }
+            return true;
+
+        }
+
+        public bool ComparacionInternaMealy(List<String> grupo, Estado evaluado)
+        {
+            var estadoComparador = this.Estados;
+            var estadoEvaluado = evaluado.Estados;
+            int contadorResultado = 0;
+            bool resultado = false;
+
+            for (int i = 0; i < estadoComparador.Count(); i++)
+            {
+                var estCo1 = estadoComparador[i];
+                var estEv1 = estadoEvaluado[i];
+
+                List<string> resultadoEstados = grupo;
+                foreach (var t in resultadoEstados)
+                {
+                    Console.WriteLine("resultado  " + t);
+
+                }
+                if (ComparadorSubEstados(resultadoEstados, grupo))
+                {
+                    contadorResultado++;
+                    Console.WriteLine("Contador resultado " + contadorResultado);
+
+                }
+                if (contadorResultado.Equals(evaluado.Estados.Count()))
+                {
+                    Console.WriteLine("Resultado = true ");
+                    resultado = true;
+                }
+            }
+
+            return resultado;
+        }
+
+        public String CompararEstadosMealy(IEnumerable<IGrouping<string, Estado>> grupo, Estado evaluado)
+        {
+            var estadoComparador = this.Estados;
+            var estadoEvaluado = evaluado.Estados;
+            int contadorResultado = 0;
+            bool resultado = false;
+
+
+            for (int i = 0; i < estadoComparador.Count(); i++)
+            {
+                var estCo1 = estadoComparador[i];
+                var estEv1 = estadoEvaluado[i];
+                bool continua = true;
+
+
+                List<string> busqueda = new List<string>() { estCo1, estEv1 };
+
+                foreach (var estados in grupo)
+                {
+
+
+                    if (estados.Count() >= busqueda.Count() && continua)
+                    {
+                        List<string> resultadoEstados = estados.Select(e => e.estado).ToList();
+
+                        foreach (var t in resultadoEstados)
+                        {
+                            Console.WriteLine("resultado  " + t);
+
+                        }
+
+                        if (ComparadorSubEstados(resultadoEstados, busqueda))
+                        {
+                            contadorResultado++;
+                            Console.WriteLine("Contador resultado " + contadorResultado);
+                            continua = false;
+                        }
+                        else
+                        {
+                            if (estadoPendiente == null)
+                            {
+                                estadoPendiente = evaluado;
+
+                                Console.WriteLine("estadoPendiente " + evaluado.estado);
+                            }
+                            else if (!estadoPendiente.estado.Equals(evaluado.estado))
+                            {
+                                Console.WriteLine("Entré");
+                                var est = estadoPendiente.Estados.ToList();
+                                if (estadoPendiente.ComparacionInterna(est, evaluado))
+                                {
+                                    Console.WriteLine("EVALUADO NAME " + evaluado.estado);
+                                    contadorResultado = 2;
+
+                                    // NombreGrupo = estado;
+
+                                    break;
+
+                                }
+                            }
+
+
+                        }
+                    }
+                }
+                if (contadorResultado.Equals(Salidas.Count()))
+                {
+                    Console.WriteLine("Resultado = true " + estCo1 + " , " + estEv1);
+                    resultado = true;
+                }
+
+            }
+            if (resultado == true)
+            {
+                evaluado.NombreGrupo = estado;
+                NombreGrupo = estado;
+            }
+            else
+            {
+                evaluado.NombreGrupo = evaluado.estado;
+            }
+
+            return evaluado.NombreGrupo;
+        }
+        public bool ComparacionInterna(List<String> grupo, Estado evaluado)
+        {
+            var estadoComparador = this.Estados;
+            var estadoEvaluado = evaluado.Estados;
+            int contadorResultado = 0;
+            bool resultado = false;
+
+            for (int i = 0; i < estadoComparador.Count(); i++)
+            {
+                var estCo1 = estadoComparador[i];
+                var estEv1 = estadoEvaluado[i];
+
+                List<string> resultadoEstados = grupo;
+                foreach (var t in resultadoEstados)
+                {
+                    Console.WriteLine("resultado  " + t);
+
+                }
+                if (ComparadorSubEstados(resultadoEstados, grupo))
+                {
+                    contadorResultado++;
+                    Console.WriteLine("Contador resultado " + contadorResultado);
+
+                }
+                if (contadorResultado == 2)
+                {
+                    Console.WriteLine("Resultado = true ");
+                    resultado = true;
+                }
+            }
+
+            return resultado;
+        }
+
+        public String CompararEstadosMoore(IEnumerable<IGrouping<string, Estado>> grupo, Estado evaluado)
+        {
+            var estadoComparador = this.Estados;
+            var estadoEvaluado = evaluado.Estados;
+            int contadorResultado = 0;
+            bool resultado = false;
+
+
+            for (int i = 0; i < estadoComparador.Count() && !resultado; i++)
+            {
+                var estCo1 = estadoComparador[i];
+                var estEv1 = estadoEvaluado[i];
+                bool continua = true;
+
+                Console.WriteLine("Cambio de busqueda");
+                List<string> busqueda = new List<string>() { estCo1, estEv1 };
+                Console.WriteLine("estados " + estCo1 + " , " + estEv1);
+
+                foreach (var estados in grupo)
+                {
+                    if (estados.Count() >= busqueda.Count() && continua)
+                    {
+                        List<string> resultadoEstados = estados.Select(e => e.estado).ToList();
+
+                        foreach (var t in resultadoEstados)
+                        {
+                            Console.WriteLine("resultado  " + t);
+
+                        }
+
+                        if (ComparadorSubEstados(resultadoEstados, busqueda))
+                        {
+                            contadorResultado++;
+                            Console.WriteLine("Contador resultado " + contadorResultado);
+                            continua = false;
+                        }
+                        else
+                        {
+                            if (estadoPendiente == null)
+                            {
+                                estadoPendiente = evaluado;
+
+                                Console.WriteLine("estadoPendiente " + evaluado.estado);
+                            }
+                            else if (!estadoPendiente.estado.Equals(evaluado.estado))
+                            {
+                                Console.WriteLine("Entré");
+                                var est = estadoPendiente.Estados.ToList();
+                                if (estadoPendiente.ComparacionInterna(est, evaluado))
+                                {
+                                    Console.WriteLine("EVALUADO NAME " + evaluado.estado);
+                                    contadorResultado = 2;
+
+                                    // NombreGrupo = estado;
+
+                                    break;
+
+                                }
+
+
+                            }
+
+                            Console.WriteLine("Si");
+                        }
+                    }
+
+                }
+                if (contadorResultado == 2)
+                {
+                    Console.WriteLine("Resultado = true ");
+                    resultado = true;
+                }
+            }
+            if (resultado == true)
+            {
+                if (estadoPendiente == null)
+                {
+                    evaluado.NombreGrupo = estado;
+                    NombreGrupo = estado;
+
+                }
+
+
+                else if (estadoPendiente != null)
+                {
+                    evaluado.NombreGrupo = estadoPendiente.NombreGrupo;
+                    estadoPendiente = null;
+                }
+                Console.WriteLine("TRUE :Evaluado.estad " + evaluado.estado + " evaluado.nomG " + evaluado.NombreGrupo);
+            }
+            else
+            {
+                evaluado.NombreGrupo = evaluado.estado;
+                //  NombreGrupo = estado;
+                Console.WriteLine("Evaluado.estad " + evaluado.estado + " evaluado.nomG " + evaluado.NombreGrupo);
+            }
+
+            return evaluado.NombreGrupo;
+        }
+    }
+}
---
+++ equivalencia de automatas/equivalencia de automatas/Maquina.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equivalencia_de_automatas
+{
+    class Maquina
+    {
+
+        public const int Mealy = 1;
+        public const int Moore = 0;
+
+        public int type { get; set; }
+        public Estado[] Lista { get; set; }
+        public int Indice { get; set; }
+        public Maquina(Estado[] lis, int tipo)
+        {
+            Lista = lis;
+            Indice = 0;
+            type = tipo;
+        }
+
+        public void AsignarParticion(IEnumerable<IGrouping<string, Estado>> grupo)
+        {
+            if (type == Mealy)
+            {
+                ParticionPnMealy(grupo);
+
+            }
+            else
+            {
+                ParticionPnMoore(grupo);
+
+            }
+
+        }
+        public IEnumerable<IGrouping<string, Estado>> ParticionP0()
+        {
+            foreach (var n in Lista)
+            {
+                var v1 = n.Salidas;
+                // Console.WriteLine("Estado de actual" +n.estado);
+
+                foreach (var n2 in Lista)
+                {
+                    if (!n2.estado.Equals(n.estado))
+                    {
+                        var v2 = n2.Salidas;
+                        // Console.WriteLine("Estado temp " + n2.estado);
+
+                        if (n.CompararSalidas(v2) == true)
+                        {
+                            //Console.WriteLine("Salidas iguales " +n.estado+ " y " + n2.estado);
+                            n.NombreGrupo = v1[0] + "";
+                        }
+
+                    }
+
+                }
+                if (n.NombreGrupo.Equals(""))
+                {
+                    n.NombreGrupo = n.estado;
+                }
+
+            }
+            var p1 = Lista.GroupBy(m => m.NombreGrupo);
+            return p1;
+        }
+
+        public void ParticionPnMealy(IEnumerable<IGrouping<string, Estado>> p1)
+        {
+            foreach (var e in p1)
+            {
+                var comparador = e.First();
+
+                foreach (var evaluado in e)
+                {
+                    if (!evaluado.estado.Equals(comparador.estado))
+                    {
+
+                     //   Console.WriteLine("Evaluado " + evaluado.estado);
+                     //   Console.WriteLine("Comparador " + comparador.estado);
+
+                        comparador.CompararEstadosMealy(p1, evaluado);
+
+
+                    }
+                }
+
+            }
+        }
+
+        public void ParticionPnMoore(IEnumerable<IGrouping<string, Estado>> p1)
+        {
+            foreach (var e in p1)
+            {
+                var comparador = e.First();
+
+                foreach (var evaluado in e)
+                {
+                    if (!evaluado.estado.Equals(comparador.estado))
+                    {
+
+                     //   Console.WriteLine("Evaluado " + evaluado.estado);
+                       // Console.WriteLine("Comparador " + comparador.estado);
+
+                        comparador.CompararEstadosMoore(p1, evaluado);
+
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
